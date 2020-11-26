b6b51a12186c4dcee459fe899efe633f3b20c4cb
fr2Dev <francois.degrincourt@gmail.com>
2020-11-26 18:16:13 +0100

    [ADD] load more cards feat

--- src/components/lastExtension.tsx
+++ src/components/lastExtension.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 // Types
 import { DefaultState, Card } from '../types';
@@ -10,14 +11,35 @@ const getLastExtensionName = (extansions: {}): string => {
   return Object.keys(extansions)[Object.keys(extansions).length - 1];
 };
 
+const postsPerPage = 10;
+
 const LastExtension: React.FC<LastExtensionProps> = () => {
   const { cards, base } = useSelector((state: DefaultState) => state.infos);
   const lastStandard = base.standard[base.standard.length - 1];
   const lastExtensionName = getLastExtensionName(cards);
   const isLast = lastExtensionName.includes(lastStandard);
+  // Get last extension
   const latestExtension = cards[lastExtensionName].filter(
     (card: Card) => (card.type === 'Spell' || card.type === 'Minion') && card.img
   );
+  // Setup handler cards to show
+  const [cardsToShow, setCardsToShow] = useState([]);
+  const [next, setNext] = useState(postsPerPage);
+  const loopWithSlice = (start: number, end: number) => {
+    const slicedPosts = [...latestExtension].slice(start, end);
+    setCardsToShow([...cardsToShow, ...slicedPosts]);
+  };
+  const moreCardsAvailable = cardsToShow.length < latestExtension.length;
+
+  // Use Effect
+  useEffect(() => {
+    loopWithSlice(0, postsPerPage);
+  }, []);
+
+  const handleShowMoreCards = () => {
+    loopWithSlice(next, next + postsPerPage);
+    setNext(next + postsPerPage);
+  };
 
   return (
     <div>
@@ -25,13 +47,16 @@ const LastExtension: React.FC<LastExtensionProps> = () => {
       <h2>{lastExtensionName}</h2>
       <div>
         {isLast && (
-          <CardList>
-            {latestExtension.map((card) => {
-              const { cardId, name, img } = card;
+          <>
+            <CardList>
+              {cardsToShow.map((card) => {
+                const { cardId, name, img } = card;
 
-              return <li key={cardId}>{img && <img src={img} alt={name} />}</li>;
-            })}
-          </CardList>
+                return <li key={cardId}>{img && <img src={img} alt={name} />}</li>;
+              })}
+            </CardList>
+            {moreCardsAvailable && <button onClick={handleShowMoreCards}>Load more</button>}
+          </>
         )}
       </div>
     </div>
